3fc77162afb3ca2d93889cea682ad602be784f52
Milan Matull <milan@milanmatull.com>
2015-05-21 23:43:31 +0200

    No implicit globals.

--- js/responsive_image_styles.js
+++ js/responsive_image_styles.js
@@ -1,39 +1,50 @@
 'use strict';
-(function ($) {
+(function ($, window, document, undefined) {
 
-  Drupal.behaviors.responsiveImages = {
+  window.Drupal.behaviors.responsiveImages = {
 
     classNames: null,
 
     attachImage: function(img) {
       var $img = $(img);
       var options = JSON.parse($img.attr('data-responsive-image'));
+      var mri; // REVIEW: What is 'mri'? my responsive image?
+
       options.classNames = this.classNames;
-      var mri = new ResponsiveImage($img, options);
-      $img.data('mri',mri);
+      mri = new window.ResponsiveImage($img, options);
+      $img.data('mri', mri);
     },
 
-    attach: function (context, settings) {
+    attach: function (context) {
+
+      var isHandledClass;
+
       // create singleton:
-      if (!Drupal.viewportSingleton) {
-        Drupal.viewportSingleton = new ViewportSingleton();
+      if (!window.Drupal.viewportSingleton) {
+        window.Drupal.viewportSingleton = new window.ViewportSingleton();
       }
 
       // get class-names from Drupal settings
+      // REVIEW: default and options extension.
       if (!this.classNames) {
-        this.classNames = RI_CLASSNAMES;
-        if (Drupal.settings.responsive_image_styles.classNames) {
-          jQuery.extend(this.classNames, Drupal.settings.responsive_image_styles.classNames);
+        this.classNames = window.RI_CLASSNAMES;
+        if (window.Drupal.settings.responsive_image_styles.classNames) {
+          $.extend(
+            this.classNames,
+            window.Drupal.settings.responsive_image_styles.classNames
+          );
         }
       }
 
-      var isHandledClass = this.classNames.STATE.ELEMENT_IS_HANDLED;
-      $('img[data-responsive-image]:not(.'+isHandledClass+')', context).addClass(isHandledClass).each(function(ndx, elem) {
-        this.attachImage(elem);
-      }.bind(this));
+      isHandledClass = this.classNames.STATE.ELEMENT_IS_HANDLED;
 
-      // Check viewport.
-      Drupal.viewportSingleton.update();
+      $('img[data-responsive-image]:not(.' + isHandledClass + ')', context)
+        .addClass(isHandledClass)
+        .each($.proxy(function(index, element) {
+          this.attachImage(element);
+        }, this));
+
+      window.Drupal.viewportSingleton.update(); // Check viewport.
     }
   };
-})(jQuery);
+})(jQuery, window, document);
